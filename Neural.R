fd83503b2a4879d5f633e8acc6cac452d4d2ccc2
Sayand Bhattacharya <sayand97@gmail.com>
2017-10-11 14:47:52 +0530

    Add files via upload

---
+++ Neural.R
@@ -0,0 +1,35 @@
+#Importing Liibraries
+library(neuralnet)
+library(nnet)
+library(ggplot2)
+
+#Importing dataset
+data <- datasets::iris
+
+#One Hot Encoding!
+data <- cbind(data[,1:4],class.ind(data$Species))
+
+#Sampling
+set.seed(1234)
+index <- sample(1:nrow(data), replace = TRUE)
+train <- data[index,]
+test <- data[-index,]
+
+#Model Fitting
+fit <- neuralnet(setosa + versicolor + virginica ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width, data = train, hidden = c(5,5), threshold = 0.01,
+                 stepmax = 1e+05, algorithm = "rprop+",
+                 err.fct = "sse", act.fct = "logistic")
+#Plotting Model
+plot(fit,col.entry="blue",col.hidden="green",col.out = "red")
+
+#Cross Validation
+sol <- round(compute(fit, test[,1:(ncol(test)-3)])$net.result)
+t1 <- sol
+t1[,1] <- abs(t1[,1]-test$setosa)
+t1[,2] <- abs(t1[,2]-test$versicolor)
+t1[,3] <- abs(t1[,3]-test$virginica)
+
+#Accuracy
+err <- sum(t1==1)
+acc <- ((nrow(t1)-err)/(nrow(t1)))*100
+acc
